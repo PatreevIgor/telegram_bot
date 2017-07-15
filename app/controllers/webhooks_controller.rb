c5b06f8db24a8cbb32565eb6c39dda5fdc4f99f6
lgor89 <igor.penkovsky@gmail.com>
2017-07-16 00:34:38 +0300

    fix bugs

--- app/controllers/webhooks_controller.rb
+++ app/controllers/webhooks_controller.rb
@@ -1,15 +1,14 @@
 require 'net/http'
 class WebhooksController < ApplicationController
   skip_before_action :verify_authenticity_token
+  COMMON_REQUEST = Rails.application.secret.common_request
   def callback
-    @bot_token = Rails.application.secrets.telegram_token
     @response = params[:message][:text]
-    @common_request = "https://api.telegram.org/bot#{@bot_token}/"
     @chat_id = params[:message][:chat][:id]
+    @name = params[:message][:from][:first_name]).to_s)
     if @response.casecmp('start').zero?
-      uri = URI(@common_request + 'sendMessage' + '?chat_id=' + 
-            @chat_id.to_s + '&' + 'text=' + 'hello ' + (
-	    params[:message][:from][:first_name]).to_s)
+      parameters = URI.encode_www_form("chat_id"=>@chat_id.to_s,"text"=>"hello #{@name}")
+      uri = URI(COMMON_REQUEST + 'sendMessage?'+"#{parameters}")
       @res = Net::HTTP.post_form(uri, 'q' => '')
     end
   end
--- config/initializers/bot.rb
+++ config/initializers/bot.rb
@@ -2,18 +2,11 @@ require 'net/http'
 require 'json'
 
 BOT_TOKEN = Rails.application.secrets.telegram_token
-COMMON_REQUEST = "https://api.telegram.org/bot#{BOT_TOKEN}/".freeze
-URL = 'https://arcane-savannah-59671.herokuapp.com/'.freeze
+COMMON_REQUEST = Rails.application.secrets.telegram_token
+URL = Rails.application.secrets.url
 def set_webhook
   uri = URI(COMMON_REQUEST + 'setWebhook' + '?url=' + URL.to_s)
   @res = Net::HTTP.get(uri)
   p @result = JSON.parse(@res)
 end
-
-def get_me
-  uri = URI(COMMON_REQEST + 'getMe/')
-  @res = Net::HTTP.get(uri)
-  p @result = JSON.parse(@res)
-end
-
 set_webhook
